89975f99892847e9010e099f297b05d72a135369
Ecem <77411067+ecemiinngg@users.noreply.github.com>
2022-02-20 19:44:23 +0300

    Add files via upload

---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace console {
+
+    class Program {
+
+        static void Main( string[] args) {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("isminizi giriniz");
+            var name = Console.ReadLine();
+            Console.WriteLine("hosgeldin"  + name + " ");
+
+        }
+    }
+}
+
+
+
